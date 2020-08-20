72411a68e896aa942bfa186ac5e7f8b80dee4f7e
andres-sws <42388101+andres-sws@users.noreply.github.com>
2020-08-20 10:51:18 -0500

    Update ejemplo.cpp

--- Documentos/clase1/CC1233192753/ejemplo.cpp
+++ Documentos/clase1/CC1233192753/ejemplo.cpp
@@ -11,10 +11,10 @@ int main() {
    printf("Insterte un entero n2");
    scanf("%d", &n2);
    n = (n2 - n1)+1;
-   f = n/2.0
-   s = (n1 + n2)*(n/2);
-   printf("La suma desde %d hasta %d es %d", n1, n2, s);
-   
-   
+   f = n/2.0 ;
+   s = (n1 + n2)*f;
+   printf("La suma desde %d hasta %d es %f", n1, n2, s);
+
+
    return 0;
 }
