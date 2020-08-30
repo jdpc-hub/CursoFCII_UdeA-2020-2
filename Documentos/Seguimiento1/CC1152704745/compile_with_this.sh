a361a3f3f392a7ff3350d3cad7f444bacea9bbb4
Jumarulanda <juan.marulandas@udea.edu.co>
2020-08-30 11:33:53 -0500

    Edit bash compilation script

--- Documentos/Seguimiento1/CC1152704745/compile_with_this.sh
+++ Documentos/Seguimiento1/CC1152704745/compile_with_this.sh
@@ -1,5 +1,5 @@
 echo "Compilando programa para calcular valores individuales de la FDP Normal, dada la media y la desviación estándar"
-g++ gaussbell.cpp -o gaussbell_plot.out
+g++ gaussbell.cpp -o gaussbell.out
 echo "listo"
 
 echo "Compilando programa para graficar la FDP Normal a una desv, estándar, dada la media y la desviación estándar"
