9ed4be055f9b94feadc50081bf9cf1c7bf211eb0
maryialejandra <maryi.carvajal@udea.edu.co>
2020-09-08 23:25:31 -0500

    Tarea2

---
+++ Documentos/Seguimiento1/CC1037666148/Tarea2/exponencial.h
@@ -0,0 +1,25 @@
+
+
+using namespace std;
+
+class D_Cal_exp
+{
+ public:
+
+  D_Cal_exp(int,float);
+  // D_Cal_exp(int);
+
+  void Set_Values(int,float);
+  //void Set_Values(int);
+  void factorial(int);
+  void expon();
+  void report_exp();
+ private:
+  int n;
+  float x;
+  int fac_n;
+  float exp;
+  
+};
+
+  
