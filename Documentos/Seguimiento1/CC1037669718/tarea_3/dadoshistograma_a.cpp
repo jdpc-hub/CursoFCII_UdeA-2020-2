e0fe6a8962c4a0642f606985c54a24e12775867e
santiagobusta <santiago.bustamanteq@udea.edu.co>
2020-09-20 00:30:39 -0500

    Tarea3

---
+++ Documentos/Seguimiento1/CC1037669718/tarea_3/dadoshistograma_a.cpp
@@ -0,0 +1,34 @@
+/*
+  Tarea 3
+  Santiago Bustamante
+*/
+
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+  const unsigned int N = 36000; // number of throws
+  unsigned int dice1, dice2, sum;
+  unsigned int frequency[11] = {}; // sum frequency from 2 to 12
+
+  srand(time(NULL));
+
+  for(int i = 0; i < N; i++){
+    dice1 = 1 + rand()%6;
+    dice2 = 1 + rand()%6;
+    sum = dice1 + dice2;
+
+    frequency[sum - 2]++;
+  }
+
+  cout << "| SUM | FREQUENCY | PERCENTAGE |\n";
+  for(int i = 0; i < 11; i++){
+    cout << "|" << setw(5) << i + 2 << "|" << setw(11) << frequency[i] << "|" << setw(12) << setprecision(2) << fixed << float(frequency[i])/float(N)*100 << "|\n";
+  }
+  return 0;
+}
+  
