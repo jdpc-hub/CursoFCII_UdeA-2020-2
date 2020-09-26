4d1086e01e5ad0dda614aab79c845b3dcc982b9c
Jumarulanda <juan.marulandas@udea.edu.co>
2020-09-26 14:05:07 -0500

    Add complete exam

--- Documentos/Parcial1/CC1152704745/punto3/trap.cpp
+++ Documentos/Parcial1/CC1152704745/punto3/trap.cpp
@@ -3,9 +3,9 @@
 double trap_int(double f (double), double a, double b, int n){
 	/* Función para calcular la integral de una función de una variable.
 		INPUTS: f - función de tipo double con entrada de tipo double
-			    a - límite inferior de integración de tipo double
- 				b - límite inferior de integración de tipo double
-				n - número de divisiones para hacer la integral, de tipo entero*/
+		        a - límite inferior de integración de tipo double
+ 		        b - límite inferior de integración de tipo double
+		        n - número de divisiones para hacer la integral, de tipo entero*/
 
 	double h = (b-a)/n;  // Base de cada trapecio. Depende del número de divisiones y de los límites
 	double t_int = 0;    // Valor inicial de la suma de la integral
@@ -16,7 +16,7 @@ double trap_int(double f (double), double a, double b, int n){
 
 		double xip1 = xi + h;                // El límite superior es el inferior + h
 		t_int += trap(f(xi), f(xip1), h);    // Se calcula el área del trapecio
-		xi = xip1;		                     // El límite inferior siguiente es el superior actual
+		xi = xip1;                           // El límite inferior siguiente es el superior actual
 	}
 
 	return t_int;
@@ -25,8 +25,8 @@ double trap_int(double f (double), double a, double b, int n){
 double trap(double fi, double fip1, double h){
 	/* Función que calcula el área de cada trapecio.
 		INPUTS: fi   - la función evaluada en el límite inferior del trapecio, de tipo double
-				fip1 - la función evaluada en el límite superior del trapecio, de tipo duouble
-				h    - la base del trapecio, de tipo double*/
+		        fip1 - la función evaluada en el límite superior del trapecio, de tipo duouble
+		        h    - la base del trapecio, de tipo double*/
 
 	double t1 = fi + fip1;
 	double t2 = t1*h;
